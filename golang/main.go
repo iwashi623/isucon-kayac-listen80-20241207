5aabfceac841d498e2651c76c070f06e4a300070
iwashi623 <wonderland.7878@gmail.com>
2024-12-14 16:13:42 +0900

    if null

--- golang/main.go
+++ golang/main.go
@@ -356,7 +356,7 @@ func getFavoritesCountByPlaylistID(ctx context.Context, db connOrTx, playlistID 
 	if err := db.GetContext(
 		ctx,
 		&count,
-		"SELECT count FROM playlist_favorite_count where playlist_id = ?",
+		"SELECT IFNULL(count, 0) FROM playlist_favorite_count where playlist_id = ?",
 		playlistID,
 	); err != nil {
 		return 0, fmt.Errorf(
